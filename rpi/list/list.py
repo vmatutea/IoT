81d0cfc9d7ef79dfec8bfcec4caa3e1402c6d418
vmatutea <vimarribas@gmail.com>
2016-01-05 13:41:02 +0100

    rpi

---
+++ rpi/list/list.py
@@ -0,0 +1,12 @@
+number1= input ("Enter a number:")
+print(number1)
+number2= input ("Enter a number:")
+print(number2)
+number3= input ("Enter a number:")
+print(number3)
+
+lst= [number1,number2,number3]
+
+lst.sort()
+
+print (lst)
